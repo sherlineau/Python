43f33c72871b50e104285c0e09b2d365c60c8aed
Sherline Au <au.sherline@gmail.com>
2022-06-07 13:51:38 -0500

    chaining methods

--- Fundamentals/OOP/User.py
+++ Fundamentals/OOP/User.py
@@ -8,48 +8,34 @@ class User:
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
         self.account_balance += amount
-        # print("Amount deposited: ", amount)
+        return self
 
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
-        # print("Amount withdrawn: ", amount)
+        return self
 
     def display_account_info(self):
         print("Name: ", self.name)
         print("Email: ", self.email)
         print("Account Balance: ", self.account_balance)
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
-        print(f"{amount} was transferred")
-        print(f"{other_user.name}'s new balance is {other_user.account_balance}")
-        
+        print(f"{self.name} transfered {amount} to {other_user.name}")
 
-
-adrian = User("Adrian", "Adrian@example.com")
+guido = User("Guido", "guido@example.com")
 
 sherline = User("Sherline", "sherline@example.com")
 
 linda = User("Linda", "linda@example.com")
 
-adrian.make_deposit(100)
-adrian.make_deposit(120)
-adrian.make_deposit(400)
-adrian.make_withdrawl(100)
-adrian.display_account_info()
+guido.make_deposit(100).make_deposit(120).make_deposit(400).make_withdrawl(100).display_account_info()
 
-sherline.make_deposit(142)
-sherline.make_deposit(75)
-sherline.make_withdrawl(30)
-sherline.make_withdrawl(200)
-sherline.display_account_info()
+sherline.make_deposit(142).make_deposit(75).make_withdrawl(30).make_withdrawl(200).display_account_info()
 
-linda.make_deposit(1000)
-linda.make_withdrawl(100)
-linda.make_withdrawl(400)
-linda.make_withdrawl(50)
-linda.display_account_info()
+linda.make_deposit(1000).make_withdrawl(100).make_withdrawl(400).make_withdrawl(50).display_account_info()
 
 linda.transfer_money(sherline,100)
