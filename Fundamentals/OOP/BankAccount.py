b12015c0909b73f1e16d515239c77b5e52e6e823
Sherline Au <au.sherline@gmail.com>
2022-06-07 19:03:52 -0500

    user with bank acount

--- Fundamentals/OOP/BankAccount.py
+++ Fundamentals/OOP/BankAccount.py
@@ -1,10 +1,31 @@
+class User:		
+    # constructor
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0,balance=0)
+
+    # adding the deposit method
+    def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
+        self.account.deposit(amount)
+        return self
+
+
+    def make_withdrawl(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_account_info(self):
+        print("Name: ", self.name)
+        print("Email: ", self.email)
+        self.account.display_account_info()
+        return self
 
 class BankAccount:
 
     list_accounts = []
 
-    def __init__(self, name, int_rate, balance = 0):
-        self.name = name
+    def __init__(self, int_rate, balance = 0):
         self.balance = balance
         self.interest = int_rate
         BankAccount.list_accounts.append( self )
@@ -32,7 +53,10 @@ class BankAccount:
             # student represents an object
             print(f"{account.name} Balance: {account.balance:.2f}")
 
-sherline = BankAccount( "Sherline", 2.5,1000).deposit(100).deposit(200).deposit(899).withdraw(120).yield_interest()
-linda = BankAccount( "Linda" , 3 ).deposit(1000).deposit(250).withdraw(100).withdraw(10).withdraw(102).withdraw(25).yield_interest()
+# sherline = BankAccount( "Sherline", 2.5,1000).deposit(100).deposit(200).deposit(899).withdraw(120).yield_interest()
+# linda = BankAccount( "Linda" , 3 ).deposit(1000).deposit(250).withdraw(100).withdraw(10).withdraw(102).withdraw(25).yield_interest()
 
-BankAccount.print_all_accounts()
+# BankAccount.print_all_accounts()
+
+sherline = User("Sherline","sherline@email.com")
+sherline.make_deposit(1000).display_account_info()
