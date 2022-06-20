3dae06786c624311f753986e473c9db6caa43655
Sherline Au <au.sherline@gmail.com>
2022-06-20 08:51:56 -0500

    CRUD

--- FlaskMySQL/Crud/Users/server.py
+++ FlaskMySQL/Crud/Users/server.py
@@ -6,19 +6,52 @@ app = Flask(__name__)
 def index():
     return redirect('/users')
 
+# display all users
 @app.route('/users')
 def users():
     return render_template("index.html", users = User.get_all())
 
+# create new users -> renders form for new users
 @app.route('/user/new')
 def new():
-    return render_template("newUserForm.html")
+    return render_template("new_user.html")
 
 @app.route('/user/create', methods = ['POST'])
 def create():
     print(request.form)
     User.save(request.form)
     return redirect('/users')
+
+# show one user
+@app.route('/user/show/<int:id>')
+def show(id):
+    data = {
+        "id":id
+    }
+    return render_template("show_user.html" , user = User.get_one(data))
+
+# edit users
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id":id
+    }
+    return render_template("edit_user.html" , user = User.get_one(data))
     
+# update user
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+# delete user
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data = {
+        "id":id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
 if __name__ == "__main__":
     app.run(debug=True)
--- FlaskMySQL/Crud/Users/user.py
+++ FlaskMySQL/Crud/Users/user.py
@@ -9,6 +9,9 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+        
     # query database, get all users
     @classmethod
     def get_all( cls ):
@@ -27,3 +30,22 @@ class User:
         result = connectToMySQL('users_schema').query_db( query, data )
         
         return result
+    
+    #retreive single user
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * from users WHERE id = %(id)s;"
+        result = connectToMySQL('users_schema').query_db( query , data)
+        return cls(result[0])
+    
+    #update user info based on id
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s, updated_at = NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+    
+    # delete user from database
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM users where id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
